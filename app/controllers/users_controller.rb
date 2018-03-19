9eb0203d7d23526a13de07f1bec17c74b31b396f
Alexander Urizar <urizaraj@gmail.com>
2018-03-19 16:11:05 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,23 @@ class UsersController < ApplicationController
   end
 
   def create
+    return redirect_to new_user_path unless confirmation_match
+
     user = User.create(strong_params)
+
+    session[:user_id] = user.id
     redirect_to welcome_path
   end
 
   def welcome
-    
+
+  end
+
+  def strong_params
+    params.require(:user).permit(*%i[name password])
+  end
+
+  def confirmation_match
+    params[:user][:password] == params[:user][:password_confirmation]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+  get '/welcome', to: 'users#welcome'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
