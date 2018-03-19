4f02b9632c5f11912e0b78524e8b4edfe90d16ee
Alexander Urizar <urizaraj@gmail.com>
2018-03-19 15:48:26 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.create(strong_params)
+    redirect_to welcome_path
+  end
+
+  def welcome
+    
+  end
 end
