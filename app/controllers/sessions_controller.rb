9b601145ac3ce79f44608de7bb67b6b8083c4182
Alexander Urizar <urizaraj@gmail.com>
2018-03-19 16:22:11 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:name])
 
-    return redirect_to :back unless user
+    return redirect_to login_path unless user
 
     authenticated = user.try(:authenticate, params[:password])
 
-    return redirect_to :back unless authenticated
+    return redirect_to login_path unless authenticated
 
     session[:user_id] = user_id
 
