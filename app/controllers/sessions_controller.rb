c7e2333ed3e5adc174a4507e80dae81fa1969e73
Alexander Urizar <urizaraj@gmail.com>
2018-03-19 16:20:36 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,13 @@
 class SessionsController < ApplicationController
-  
+  def create
+    user = User.find_by(name: params[:name])
+
+    return redirect_to :back unless user
+
+    authenticated = user.try(:authenticate, params[:password])
+
+    return redirect_to :back unless authenticated
+
+
+  end
 end
