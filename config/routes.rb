006f2274c2cb9449e09fcfed8b1cf4ca50995c82
Alexander Urizar <urizaraj@gmail.com>
2018-03-19 16:16:51 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :users
-  
+
   get '/welcome', to: 'users#welcome'
 
-  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
 end
